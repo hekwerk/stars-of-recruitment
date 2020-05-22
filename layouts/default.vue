83fee92d616854ce683d611de3b705fc19ea36cc
Dave Ligthart <dligthart@gmail.com>
2020-05-22 10:45:31 +0200

    added netlify attribution to footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -71,7 +71,9 @@
         </v-container>
       </v-content>
       <v-footer app>
-        <!-- -->
+        <a href="https://www.netlify.com">
+         <img src="https://www.netlify.com/img/global/badges/netlify-color-accent.svg" alt="Deploys by Netlify" />
+       </a>
       </v-footer>
   </v-app>
 </template>
